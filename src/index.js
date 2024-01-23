1f6098c07603bc7ab55e10007ba1f1e551900b74
viola-delia <viola.delia28@gmail.com>
2024-01-24 09:15:19 +1000

    Added API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
+function displayRecipe(response) {
+ 
+console.log("recipe generated");
+
+  new Typewriter('#recipe', {
+  strings: response.data.answer,
+  autoStart: true,
+  delay: 1,
+  cursor: "",
+});
+}
+
 function generateRecipe(event) {
 event.preventDefault();
 
-new Typewriter('#recipe', {
-  strings: ['tiramisu'],
-  autoStart: true,
-  cursor: "",
-});
+//Build the API URL
+
+let instructionsInput = document.querySelector("#instructions");
+let apiKey = "35te54ac48ff3dd030f2e92oaf3b5d78";
+let prompt = `Please generate the recipe of ${instructionsInput.value} including all the different steps and ingredients. Show the recipe in basic HTML for a better UX`;
+let context = "You are a master chef with a lot of knowledge in recipes from all over the world";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+console.log(`Prompt : ${prompt}`);
+
+//make a call to the API using AXIOS
+
+axios.get(apiUrl).then(displayRecipe);
+
+// DIsplay the generated recipe
+
+
 
 
 }
